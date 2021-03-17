d1b33f73b33fe70ef84bf23c0d976ff23cb55333
Seokhyun Kim <wjapp72@gmail.com>
2021-03-17 11:06:49 -0700

    #Combine Example and TextField match

---
+++ combine_textfield_tutorial/MyViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  MyViewModel.swift
+//  combine_textfield_tutorial
+//
+//  Created by Seokhyun Kim on 2021-03-17.
+//
+
+import Foundation
+import Combine
+
+class MyViewModel {
+    //published 어노테이션을 통해 구독이 가능하도록 설정.
+    @Published var passwordInput : String = "" {
+        didSet {
+            print("MyViewModel / passwordInput : \(passwordInput)")
+        }
+    }
+    @Published var passwordConfirmInput : String = "" {
+        didSet {
+            print("MyViewModel / passwordConfirmInput : \(passwordConfirmInput)")
+        }
+    }
+    
+    //들어온 퍼블리셔들의 값 일치 여부를 반환하는 퍼블리셔
+    lazy var isMatchPasswordInput : AnyPublisher<Bool, Never> = Publishers.CombineLatest($passwordInput, $passwordConfirmInput)
+        .map({ (password: String, passwordConfirm: String) in
+            if password == "" || passwordConfirm == "" {
+                return false
+            }
+            if password == passwordConfirm {
+                return true
+            } else {
+                return false
+            }
+            
+        })
+        .print()
+        .eraseToAnyPublisher()
+}
--- combine_textfield_tutorial/ViewController.swift
+++ combine_textfield_tutorial/ViewController.swift
@@ -6,14 +6,69 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmTextField: UITextField!
+    @IBOutlet weak var myBtn: UIButton!
+    
+    var viewModel : MyViewModel!
+    
+    private var mySubscriptions = Set<AnyCancellable>()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewModel = MyViewModel()
+        //택스트필드에서 나가는 이벤트를 뷰모델의 프로퍼티가 구독
+        passwordTextField.myTextPublisher
+ //           .print()
+            //메인 쓰레드에서 받는다.
+            .receive(on: DispatchQueue.main)
+            //구독
+            .assign(to: \.passwordInput, on: viewModel)
+            .store(in: &mySubscriptions)
+        
+        passwordConfirmTextField.myTextPublisher
+ //           .print()
+            //메인 쓰레드에서 받는다.
+            .receive(on: RunLoop.main)
+            //구독
+            .assign(to: \.passwordConfirmInput, on: viewModel)
+            .store(in: &mySubscriptions)
+        
+        //버튼이 뷰모델의 퍼블리셔를 구독
+        viewModel.isMatchPasswordInput
+            .print()
+            .receive(on: RunLoop.main)
+            //구독
+            .assign(to: \.isValid, on: myBtn)
+            .store(in: &mySubscriptions)
     }
 
 
 }
 
+extension UITextField {
+    var myTextPublisher : AnyPublisher<String, Never> {
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+            //UITextField 가져옴.
+            .compactMap{ $0.object as? UITextField }
+            //String `가져옴
+            .map{ $0.text ?? "" }
+            .eraseToAnyPublisher()
+    }
+}
+
+extension UIButton {
+    var isValid: Bool {
+        get {
+            backgroundColor == .yellow
+        }
+        set {
+            backgroundColor = newValue ? .yellow : .lightGray
+            isEnabled = newValue
+            setTitleColor(newValue ? .blue : .white, for: .normal)
+        }
+    }
+}
